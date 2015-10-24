22e4948f19648e32cf9f1d459963d612a18abbe6
nguyetduong.minh@gmail.com <nguyetduong.minh@gmail.com>
2015-10-24 01:52:41 -0700

    test files to see if the app works, and the introduction of the server

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Simple website
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
---
+++ server.py
@@ -0,0 +1,4 @@
+## Server to send appropriate messages to subscribers
+
+from flask import Flask, request, redirect
+app = Flask(__Name__)n
