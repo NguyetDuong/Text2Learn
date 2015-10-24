90f69e4802b94107f523000361f5140c89e20181
nguyetduong.minh@gmail.com <nguyetduong.minh@gmail.com>
2015-10-24 03:37:25 -0700

    Registered what we input is working, needs to be able to store/remember phone numbers

--- server.py
+++ server.py
@@ -1,4 +1,44 @@
-## Server to send appropriate messages to subscribers
-
 from flask import Flask, request, redirect
-app = Flask(__Name__)n
+import twilio.twiml
+ 
+app = Flask(__name__)
+name = "Text2Learn"
+subscribeMessage = "make it easier"
+errorMessage = "To subscribe, please reply with MAKE IT EASIER"
+subscribers = []
+
+def parseSubscription(inp):
+	"""Takes in a String, and will parse it to see if it contains the phrase
+	   to subscribe to our Text2Learn."""
+
+	l = inp.lower()
+	
+	b = (l == subscribeMessage)
+	return b
+
+
+# App is being run at this point, all variables & methods 
+# not related to app should be above
+@app.route("/", methods=['GET', 'POST'])
+def subscribe():
+	"""Reads the incoming messages to see who is subscribing."""
+	automatic_subscription = "You are now subscribed to " + name +". To learn the key functions, text HELP back."
+	body_message = request.values.get('Body', None)
+	person_number = request.values.get('From', None)
+	if parseSubscription(body_message):
+		resp = twilio.twiml.Response()
+		resp.message(automatic_subscription)
+		subscribers.append(person_number)
+	else:
+		resp = twilio.twiml.Response()
+		resp.message(errorMessage)
+	
+
+	return str(resp)
+
+
+
+
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
