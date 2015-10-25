e7b28654bf4ecc4c8df0b8d0a2541767eb37cfcf
KatzeWire <jscalett@ucsc.edu>
2015-10-24 17:47:42 -0700

    Cleaned up files and added comments
    Mostly cleaned up database.py:
    -got rid of unneeded code
    -added comments
    -left some test code

--- database.py
+++ database.py
@@ -1,64 +1,60 @@
 #database.py
 #Python script used to generate the database of all the quizzes and answers
-#-------------------------------------------------------------------
+#------------------------------------------------------------------------------
 #Database file name: text2learn.db
 # Ordering of table:
 # | Id | Question | Answer|
 # Current tables:
 # math
 # spanish
-#------------------------------------------
+#------------------------------------------------------------------------------
 #CONTRIBUTERS: Sarah Borland, Joyce Scalettar, Hui Shi
-
 
 import sqlite3 as lite
 import sys
 from array import array
 from sys import argv
 
-#arguments 
+#--- ARGUMENTS ----------------------------------------------------------------
 script, filename = argv
 
-
+#--- STRUCT FOR QUIZ OBJECT ---------------------------------------------------
+#Has a question array and an answer array
 class quiz(object):
     def __init__(self, title):
         self.title = title
     qArray=[None]*101
     aArray=[None]*101
-    nArray=[None]*101
-    numQuiz=0
-  
- 
-#for now, I have it using preset arrays, my next plan is to have it intake a textfile
-#and automatically create these arrays for ease of editing yaaaa <3
-#mathArr=[(1,'5+9=?','14' ),
-#		    (2, '3*3=?','9'),
-#			(3, '8/2=?','4'),
-#		  ]
-		   
-#spanishArr=[(1, 'What is the cat in Spanish?','el gato' ),
-#		    (2, 'What is bread in Spanish?','el pan'),
-#			(3, 'What is programmer in Spanish?','programador'),
-#		    ]
-
-
+#Initialization of quiz object ------------------------------------------------
 quizObject = quiz(["title"])
 
+
 try:
+	#Connect to database file -------------------------------------------------
     con = lite.connect('text2learn.db')
     cur = con.cursor()  
     file = open(filename)
     lineNum = 0
-    x =1
+    x = 1
+	
+	#Go through to add lines to table -----------------------------------------
     while True:
         nextLine = file.readline()
+		
+		#Break from while when end of txt file is reached ---------------------
         if nextLine=='':
-             print 'END OF FILE'
+             #print 'END OF FILE'
              break
+			 
+		#New line means that there will be a next table label and, ------------
+		#if there has already been one table's worth of data scanned into 
+		#the quiz object, a table will be created 
         if nextLine=='\n':
-            print 'NEW LABEL'
+            #print 'NEW LABEL'
+			
+			#A table's worth of data has been scanned, make table -------------
             if lineNum>0:
-                print 'PUTTING IN TABLE'
+                #print 'PUTTING IN TABLE'
                 QuizArr=[None]*lineNum
                 for n in range(0, lineNum):
                     QuizArr[n] =((n), (quizObject.qArray[n]), (quizObject.aArray[n])) 
@@ -68,64 +64,45 @@ try:
                 #print categories
                 #cur.execute('''DROP TABLE IF EXISTS '''+categories)
                 cur.execute("DROP TABLE IF EXISTS "+categories)
-                #print "making table" 
+                #print "making table"
+				
+				#The table holds the question ID, the question itself, and then the answer
+				#in each column. When grabbing data from the database, it should (hopefully!)
+				#have it's datatype already set.
                 cur.execute("CREATE TABLE "+categories+"(Id INT, Name TEXT, Answer TEXT)")  
-                
-                #print lineNum                
-                #for n in range(0,lineNum):
-                 #   cur.execute('''INSERT INTO '''+categories+'''(Id, Question, Answer) VALUES(?, ?, ?)''', (n, quizObject.qArray[n], quizObject.aArray[n],))
-                
                 cur.executemany("INSERT INTO "+categories+" VALUES(?, ?, ?)", QuizArr)
-                
-                #cur.execute("INSERT INTO math VALUES (?,?,?)", (QuizArr,))
-                
-
-
-                #print QuizArr
-                #stmt = "insert into " + categories + " (Id, Question, Answer) values (?, ?, ?)"   
-                #cur.executemany(stmt, QuizArr)
-                #print cur.rowcount
-
-            print "finished if statement"
-            cur.execute("SELECT * FROM math")
+            #print "finished if statement"
+            #cur.execute("SELECT * FROM math")
             #print cur.rowcount
-            print "+++++++++++++++++++++++++++"
-            print cur.fetchone()
-            print "/+++++++++++++++++++++++++"
-            lineNum=0
-            print "finished if statement"
-            cur.execute("SELECT * FROM spanish")
+            #print "+++++++++++++++++++++++++++"
+            #print cur.fetchone()
+            #print "/+++++++++++++++++++++++++"
+            #lineNum=0
+            #print "finished if statement"
+            #cur.execute("SELECT * FROM spanish")
             #print cur.rowcount
-            print "+++++++++++++++++++++++++++"
-            print cur.fetchone()
-            print "/+++++++++++++++++++++++++"
+            #print "+++++++++++++++++++++++++++"
+            #print cur.fetchone()
+            #print "/+++++++++++++++++++++++++"
+			
+			#Read in new table's label and reset table variables --------------
             lineNum=0
-            #quizObject.numQuiz=quizObject.numQuiz+1
             quizName=file.readline()
-           # getattr(quizzes,quizName)
             quizObject.title = quizName
-            print 'GETTING QUIZ NAME: '+quizObject.title
+            #print 'GETTING QUIZ NAME: '+quizObject.title
             #print quizObject.title 
+		#Reading in the questions and answers into the quiz object arrays -----
         else:
-           # print 'ENTERING ELSE'
-           
+            #print 'ENTERING ELSE'
             if x==1:
                 x = 2
+				#Save line from file into question array ----------------------
                 quizObject.qArray[lineNum] = nextLine.rstrip('\n')
             else:
                 x = 1
+				#Save line from file into answer array ------------------------
                 quizObject.aArray[lineNum] = nextLine.rstrip('\n')
                 lineNum=lineNum+1
-            #print x
-           # print nextLine
-
-  
-#The table holds the question ID, the question itself, and then the answer
-#in each column. When grabbing data from the database, it should (hopefully!)
-#have it's datatype already set.
-
-
-
     con.commit()
 
 	#testing
@@ -133,9 +110,6 @@ try:
     print(cur.fetchall())
     cur.execute("SELECT * FROM spanish")
     print(cur.fetchall())
-    
- 
-
 
 #Error handling: prints out error location
 except lite.Error, e:
@@ -143,8 +117,6 @@ except lite.Error, e:
         con.rollback()
     print "Error %s:" % e.args[0]
     sys.exit(1)
-
-
 
 #Closes connection   
 finally:
