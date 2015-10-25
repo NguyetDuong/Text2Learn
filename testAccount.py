87d94638fad2becc0b2c1152a6f6d520355f3290
KatzeWire <jscalett@ucsc.edu>
2015-10-24 23:10:28 -0700

    Created Account Management Functions
    Account_Management.py:
    -adds user info into account database
    -returns question string when user requests a question
    testAccount.py:
    -tests Account_Management.py when run

---
+++ testAccount.py
@@ -0,0 +1,8 @@
+#test account shit
+
+import Account_Management
+
+Account_Management.init_subscribe('+1747629273')
+Account_Management.init_subscribe('+124')
+Account_Management.send_problem('+1747629273','learn math')
+Account_Management.send_problem('+124','learn spanish')
